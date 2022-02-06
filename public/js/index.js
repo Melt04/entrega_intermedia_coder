d17b41898d357fe80f22747ccc3a116834f7ec2d
Alan Garcia <alanmdq14@gmail.com>
2022-02-06 11:54:48 -0300

    Added chat socket

--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,34 @@
 const socket = io();
-function sortProducts(product) {
-  product.reduce((prev, curret) => {
-    1;
-  }, {});
-}
+
 async function renderProductTable(products) {
   const data = await fetch("http://localhost:8080/api/getPlantilla");
   const html = await data.text();
   var template = Handlebars.compile(html);
   const table = document.querySelector("#product-table");
-  console.log(table);
-  console.log(products);
   table.innerHTML = template({ products });
 }
 
+async function renderMessages(messages) {
+  let allMessages = "";
+  const data = await fetch("http://localhost:8080/plantilla/mensajes.hbs");
+  const html = await data.text();
+  const template = Handlebars.compile(html);
+  messages.forEach((message) => {
+    allMessages = allMessages.concat(template({ message }));
+  });
+  const messageDiv = document.querySelector("#message");
+  messageDiv.innerHTML = allMessages;
+}
 socket.on("products", (products) => renderProductTable(products));
 socket.on("update-products", (products) => renderProductTable(products));
+socket.on("messages", (data) => {
+  renderMessages(data);
+});
+const form = document.querySelector(".message_main_form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const mensaje = document.querySelector("#email_input").value;
+  const user = document.querySelector("#message_send").value;
+
+  socket.emit("new-message", { mensaje, user });
+});
